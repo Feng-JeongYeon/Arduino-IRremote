db3bae248492153b8c08085c33e31121215c8174
Feng-JeongYeon <75206140+Feng-JeongYeon@users.noreply.github.com>
2021-08-13 15:36:30 +0800

    Update IRremoteInt.h

--- src/IRremoteInt.h
+++ src/IRremoteInt.h
@@ -35,7 +35,7 @@
 #include <Arduino.h>
 
 #if ! defined(RAW_BUFFER_LENGTH)
-#define RAW_BUFFER_LENGTH  100  ///< Maximum length of raw duration buffer. Must be even. 100 supports up to 48 bit codings inclusive 1 start and 1 stop bit.
+#define RAW_BUFFER_LENGTH  255  ///< Maximum length of raw duration buffer. Must be even. 100 supports up to 48 bit codings inclusive 1 start and 1 stop bit.
 #endif
 #if RAW_BUFFER_LENGTH % 2 == 1
 #error RAW_BUFFER_LENGTH must be even, since the array consists of space / mark pairs.
